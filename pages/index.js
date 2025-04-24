cc74dc5a57b62e3a5789ce2fb1e43dae764f8614
marcelorodrigues <marcelo.vitor@gmail.com>
2025-04-24 23:27:40 +0000

    tentando sync na mesma versao

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-import database from "infra/database.js";
-
-async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+function Home() {
+  return <h1>frase 😎</h1>;
 }
 
-export default status;
+export default Home;
